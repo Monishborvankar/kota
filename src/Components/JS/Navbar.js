f48cf43604acef516bed03357ec38b120fd58fe1
unknown <monishborvankar@gmil.com>
2023-03-12 19:40:50 +0530

    first commit

---
+++ src/Components/JS/Navbar.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { Link } from 'react-scroll';
+// import { MdContactPage } from 'react-icons/md'
+// import { AiOutlineCustomerService } from 'react-icons/ai'
+// import { FcAbout } from 'react-icons/fc'
+// import { FcHome } from 'react-icons/fc'
+// import { BiLogIn } from 'react-icons/bi'
+import '../CSS/Navbar.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+// import Badge from '@mui/material/Badge';
+// import MailIcon from '@mui/icons-material/Mail';
+// import { RiHtml5Line } from "react-icons/ri";
+
+
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    useEffect(() => {
+        AOS.init({ duration: 1000 })
+    }, [])
+
+
+    return (
+        <div className="Navbar" id="Navbar">
+            <Link to='Home' className="nav-logo" style={{color:"white",right:"10px",padding:"20px"}}><span >COTA</span></Link>
+            <div className={`nav-items ${isOpen && "open"}`}>
+                <NavLink to="/" onClick={() => setIsOpen(!isOpen)}>Home </NavLink>
+                <Link to="About" onClick={() => setIsOpen(!isOpen)} style={{color:"white"}}>About </Link>
+                <Link to="Service" onClick={() => setIsOpen(!isOpen)} style={{color:"white"}}>Service </Link>
+                {/* <Link to="Notable" onClick={() => setIsOpen(!isOpen)}>Notable </Link> */}
+                <Link to="Contact" onClick={() => setIsOpen(!isOpen)} style={{color:"white"}}>Contact </Link>
+                <NavLink to="/Addroom" onClick={() => setIsOpen(!isOpen)}>Addroom</NavLink>
+                {/* <NavLink to="/Resume" onClick={() => setIsOpen(!isOpen)}>Resume</NavLink> */}
+                {/* <NavLink to="/Addcourse" onClick={() => setIsOpen(!isOpen)}>Addcourse</NavLink> */}
+                <NavLink to="/SimpleUser" onClick={() => setIsOpen(!isOpen)}>SignUp</NavLink>
+                {/* <NavLink to="/Notification" onClick={() => setIsOpen(!isOpen)}>
+                    Notification<Badge badgeContent={4} color="primary">
+                        <MailIcon color="action" />
+                    </Badge>
+                </NavLink>
+                <NavLink to="/ProfileUser" onClick={() => setIsOpen(!isOpen)}><b style={{height:"10px",width:"10px",borderRadius:"50%",backgroundColor:"orangered",padding:"7px"}}>NA</b></NavLink> */}
+            </div>
+            <div
+                className={`nav-toggle ${isOpen && "open"}`}
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <div className="bar"></div>
+            </div>
+        </div>
+    );
+};
+export default Navbar;
